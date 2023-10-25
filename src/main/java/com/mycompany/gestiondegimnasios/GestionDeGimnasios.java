60c6bf5a138a2c3e777b0e2629efae9482d5084f
FJavierVC <126486944+FJavierVC@users.noreply.github.com>
2023-10-25 10:42:03 -0600

    primer comit

---
+++ src/main/java/com/mycompany/gestiondegimnasios/GestionDeGimnasios.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.gestiondegimnasios;
+
+/**
+ *
+ * @author Francisco Javier Valdez Cuellar
+ * @author Jose Diego Toledo Mu;os
+ * 
+ */
+public class GestionDeGimnasios {
+
+    }
+
